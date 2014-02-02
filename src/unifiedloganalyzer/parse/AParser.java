d876ec1408b389a08a0c60169cb7c2953e2b87a7
Peter Trsko <peter.trsko@gmail.com>
2014-02-03 00:05:56 +0100

    Abstract implementation of IParser interface
    It contains callback functionality, but nothing else.

---
+++ src/unifiedloganalyzer/parse/AParser.java
@@ -0,0 +1,53 @@
+package unifiedloganalyzer.parse;
+
+import trskop.ICallback;
+
+import unifiedloganalyzer.IParser;
+import unifiedloganalyzer.ParsedData;
+import unifiedloganalyzer.utils.CallbacksManager;
+
+
+/**
+ * Abstract parser that implements callback management.
+ *
+ * @author Peter Trsko
+ */
+public abstract class AParser implements IParser
+{
+    private CallbacksManager<ParsedData> _callbacksManager = null;
+
+    // {{{ Constructors ///////////////////////////////////////////////////////
+
+    public AParser()
+    {
+        _callbacksManager = new CallbacksManager<ParsedData>();
+    }
+
+    public AParser(ICallback<ParsedData> callback)
+    {
+        this();
+
+        _callbacksManager.registerCallback(callback);
+    }
+
+    // }}} Constructors ///////////////////////////////////////////////////////
+
+    /**
+     * {@inheritDoc}
+     */
+    public void registerCallback(ICallback<ParsedData> callback)
+    {
+        _callbacksManager.registerCallback(callback);
+    }
+
+    /**
+     * Run all registered callbacks with parsedData as an argument.
+     *
+     * @param parsedData
+     *   Object passed to all registered callbacks when executed.
+     */
+    protected void runCallbacks(ParsedData parsedData)
+    {
+        _callbacksManager.runCallbacks(parsedData);
+    }
+}
