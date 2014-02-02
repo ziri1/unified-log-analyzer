adc1eb3a0d64927872566000f4896514bca96cc4
Peter Trsko <peter.trsko@gmail.com>
2014-02-02 23:44:06 +0100

    ParsedData now implements IOutputMessage
    This could be useful for piping analyzers.

--- src/unifiedloganalyzer/ParsedData.java
+++ src/unifiedloganalyzer/ParsedData.java
@@ -1,21 +1,24 @@
 package unifiedloganalyzer;
 
 import java.io.IOException;
-import java.lang.Appendable;
-import java.lang.IllegalArgumentException;
 
 import trskop.IAppendTo;
 
+import unifiedloganalyzer.IOutputMessage;
 import unifiedloganalyzer.IParsedData;
 
 
 /**
+ * Top-level wrapper for parsed data.
  *
  * @author Peter Trsko
  */
-public class ParsedData implements IParsedData
+public class ParsedData implements IParsedData, IOutputMessage
 {
-    public enum Type
+    /**
+     * Type of encapsulated data.
+     */
+    public static enum Type
     {
         PARSED_MESSAGE,
         PARSE_ERROR,
@@ -25,34 +28,131 @@ public class ParsedData implements IParsedData
     private Type _dataType;
     private IParsedData _data;
 
-    public ParsedData(Type dataType, IParsedData data)
+    // {{{ Constructors ///////////////////////////////////////////////////////
+
+    /**
+     * Constructor that produces ParsedData instances with type PARSED_MESSAGE.
+     *
+     * @param parsedData
+     *   IParsedData instance describing message in parsed form.
+     */
+    public ParsedData(IParsedData parsedData)
     {
-        if (dataType != Type.EMPTY_MESSAGE && data == null)
+        this(Type.PARSED_MESSAGE, parsedData);
+    }
+
+    /**
+     * Generic constructor for ParsedData.
+     *
+     * @param dataType
+     *   Type of message, it can be PARSED_MESSAGE, PARSE_ERROR or
+     *   EMPTY_MESSAGE.
+     *
+     * @param parsedData
+     *   IParsedData instance describing message in parsed form in case of
+     *   <code>dataType=PARSED_MESSAGE</code>; data that describe parse error
+     *   in case of <code>dataType=PARSE_ERROR</code>; other value or
+     *   <code>null</code> in case of <code>dataType=EMPTY_MESSAGE</code>;
+     */
+    public ParsedData(Type dataType, IParsedData parsedData)
+    {
+        if (dataType != Type.EMPTY_MESSAGE && parsedData == null)
         {
             throw new IllegalArgumentException("null");
         }
 
         _dataType = dataType;
-        _data = data;
+        _data = parsedData;
     }
 
+    /**
+     * Simple wrapper around ParsedData constructor that returns correctly
+     * formated empty message.
+     *
+     * @return
+     *   Empty ParsedData instance.
+     */
+    public static ParsedData emptyMessage()
+    {
+        return new ParsedData(Type.EMPTY_MESSAGE, null);
+    }
+
+    /**
+     * Simple wrapper around ParsedData constructor that returns correctly
+     * formated parse error message.
+     *
+     * @return
+     *   ParsedData instance that describes parse error.
+     */
+    public static ParsedData parseError(IParsedData data)
+    {
+        return new ParsedData(Type.PARSE_ERROR, data);
+    }
+
+    // }}} Constructors ///////////////////////////////////////////////////////
+
+    // {{{ Getters and setters ////////////////////////////////////////////////
+
+    /**
+     * Get type of message (parsed message, parse error or empty message).
+     *
+     * @return
+     *   Type of message, it can be PARSED_MESSAGE, PARSE_ERROR or
+     *   EMPTY_MESSAGE.
+     */
     public Type getType()
     {
         return _dataType;
     }
 
+    /**
+     * Get parsed data/AST.
+     *
+     * @return
+     *   IParsedData instance describing message in parsed form.
+     */
     public IParsedData getData()
     {
         return _data;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public String getOriginalMessage()
     {
         return _data.getOriginalMessage();
     }
 
+    // }}} Getters and setters ////////////////////////////////////////////////
+
+    /**
+     * {@inheritDoc}
+     */
     public void appendTo(Appendable buff) throws IOException
     {
         _data.appendTo(buff);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean messageEquals(IOutputMessage message)
+    {
+        if (message instanceof ParsedData)
+        {
+            ParsedData casted = (ParsedData)message;
+            IParsedData ourData = getData();
+            IParsedData theirData = casted.getData();
+
+            return getType() == casted.getType()
+                && getOriginalMessage() == casted.getOriginalMessage()
+                && ((ourData == null && theirData == null)
+                    // We have to make sure that we don't get
+                    // NullPointerException when calling equals.
+                    || (ourData != null && ourData.equals(theirData)));
+        }
+
+        return false;
     }
 }
