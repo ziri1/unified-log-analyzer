ea96c732d8a065dfdc98c92753fe3e531062970e
Peter Trško <peter.trsko@gmail.com>
2014-02-05 17:04:16 +0100

    Added counter and event for exit() syscall
    In addition some javadoc comments were added/imporoved, mostly to describe
    what the measured statistics actually mean.

--- src/unifiedloganalyzer/analyze/path/strace/Statistics.java
+++ src/unifiedloganalyzer/analyze/path/strace/Statistics.java
@@ -6,55 +6,146 @@ import unifiedloganalyzer.IOutputMessage;
 
 
 /**
+ * Strace path analysis statistics.
+ *
+ * This provides a lot of information about how well the analysis went and how
+ * reliable the results are.
  *
  * @author Peter Trsko
  */
 class Statistics implements IOutputMessage
-{ 
+{
     public static enum Event
     {
+        // {{{ Statistics of messages received by the analyzer. ///////////////
+
+        /**
+         * Analyzer received message with parsed syscall.
+         */
         SYSCALL,
-        IGNORED_SYSCALL,
-        IGNORED_RESUMED_SYSCALL,
+
+        /**
+         * Analyzer received message with parsed signal.
+         */
         SIGNAL,
+
+        /**
+         * Analyzer received message with parsed process status change.
+         */
         PROCESS_STATUS_CHANGE,
-        FORK_SYSCALL,
-        EXEC_SYSCALL,
-        OPEN_SYSCALL,
-        GETCWD_SYSCALL,
-        CHDIR_SYSCALL,
+
+        /**
+         * Analyzer received parse error message.
+         */
+        PARSE_ERROR,
+
+        /**
+         * Analyzer received an unknown message type.
+         */
         UNKNOWN_MESSAGE,
 
-        // Misses
+        // }}} Statistics of messages received by the analyzer. ///////////////
 
+        // {{{ More detailed syscall statistics ///////////////////////////////
+
+        /**
+         * Syscall is irrelevant for process model and analysis purpose.
+         */
+        IGNORED_SYSCALL,
+
+        /**
+         * Resumed syscall ignored, i.e. all necessary data already present in
+         * unfinished syscall.
+         */
+        IGNORED_RESUMED_SYSCALL,
+
+        /**
+         * Received fork() or clone() call.
+         */
+        FORK_SYSCALL,
+
+        /**
+         * Received exec() call.
+         */
+        EXEC_SYSCALL,
+
+        /**
+         * Received exit() or exit_group() call.
+         */
+        EXIT_SYSCALL,
+
+        /**
+         * Received open() call.
+         */
+        OPEN_SYSCALL,
+
+        /**
+         * Received getcwd() call.
+         */
+        GETCWD_SYSCALL,
+
+        /**
+         * Received chdir() call.
+         */
+        CHDIR_SYSCALL,
+
+        // }}} More detailed syscall statistics ///////////////////////////////
+
+        // {{{ Misses and model errors ////////////////////////////////////////
+
+        /**
+         * Process not found in model.
+         */
         GET_PROCESS_MISS,
+
+        /**
+         * Process not found in model while trying to remove it.
+         */
         REMOVE_PROCESS_MISS,
+
+        /**
+         * Model doesn't know working directory of a process.
+         */
         WORKING_DIRECTORY_MISS,
+
+        // }}} Misses and model errors ////////////////////////////////////////
+
+        // {{{ Parsing failures ///////////////////////////////////////////////
+
+        /**
+         * Unable to find PWD environment variable in parsed data.
+         */
         NO_PWD_ENV_VAR,
 
-        PARSE_ERROR,
+        /**
+         * Parser failed in some way, but hadn't produced PARSE_ERROR message.
+         */
         UNREPORTED_PARSING_FAILURE;
+
+        // }}} Parsing failures ///////////////////////////////////////////////
     }
 
     private int _messageCount = 0;
     private int _syscallCount = 0;
-    private int _ignoredSyscallCount = 0;
-    private int _ignoredResumedSyscallCount = 0;
     private int _signalCount = 0;
     private int _statusChangeCount = 0;
+    private int _parseErrorCount = 0;
+    private int _unknownMessageCount = 0;
+
+    private int _ignoredSyscallCount = 0;
+    private int _ignoredResumedSyscallCount = 0;
     private int _forkCount = 0;
     private int _execCount = 0;
+    private int _exitCount = 0;
     private int _openCount = 0;
     private int _getcwdCount = 0;
     private int _chdirCount = 0;
-    private int _unknownMessageCount = 0;
 
     private int _getProcessMissesCount = 0;
     private int _removeProcessMissesCount = 0;
     private int _wdMissesCount = 0;
-    private int _pwdEnvVarMissesCount = 0;
 
-    private int _parseErrorCount = 0;
+    private int _pwdEnvVarMissesCount = 0;
     private int _unreportedParsingFailureCount = 0;
 
     public void update(Event event)
@@ -84,6 +175,9 @@ class Statistics implements IOutputMessage
                 break;
             case EXEC_SYSCALL:
                 _execCount++;
+                break;
+            case EXIT_SYSCALL:
+                _exitCount++;
                 break;
             case OPEN_SYSCALL:
                 _openCount++;
@@ -123,6 +217,10 @@ class Statistics implements IOutputMessage
         return _syscallCount;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void appendTo(Appendable buff) throws IOException
     {
         buff.append(this.getClass().getName()).append(':')
@@ -140,6 +238,8 @@ class Statistics implements IOutputMessage
                         .append(Integer.toString(_forkCount))
                     .append("\n    , exec = ")
                         .append(Integer.toString(_execCount))
+                    .append("\n    , exit = ")
+                        .append(Integer.toString(_exitCount))
                     .append("\n    , open = ")
                         .append(Integer.toString(_openCount))
                     .append("\n    , getcwd = ")
@@ -169,6 +269,10 @@ class Statistics implements IOutputMessage
             .append("\n}");
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public boolean messageEquals(IOutputMessage message)
     {
         // Statistics are never equal!
