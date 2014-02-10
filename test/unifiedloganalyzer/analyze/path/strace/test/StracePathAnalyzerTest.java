690aee4ef6bfcc128de6c705508b75e6bc76f3a0
Peter Trško <peter.trsko@gmail.com>
2014-02-10 19:11:33 +0100

    Empty test for StracePathAnalyzer

---
+++ test/unifiedloganalyzer/analyze/path/strace/test/StracePathAnalyzerTest.java
@@ -0,0 +1,38 @@
+package unifiedloganalyzer.analyze.path.strace.test;
+
+import junit.framework.TestCase;
+
+
+/**
+ *
+ * @author Peter Trsko
+ */
+public class StracePathAnalyzerTest extends TestCase
+{
+    public StracePathAnalyzerTest(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws java.lang.Exception
+     */
+    @Override
+    protected void setUp() throws Exception
+    {
+        // Empty implementation.
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws java.lang.Exception
+     */
+    @Override
+    protected void tearDown() throws Exception
+    {
+        // Empty implementation.
+    }
+}
